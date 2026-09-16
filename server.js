e45de4c5e96cf0096b475e9d2a19059c58a98aca
shoojenifer9-arch <shoojenifer9@gmail.com>
2026-09-16 10:00:38 -0700

    Fix Railway port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const mysql = require("mysql2");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static(__dirname));
